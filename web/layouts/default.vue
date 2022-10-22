5accbe9865b1dd65fe0e33894c6cb03c35e4649b
svan-derp <stijnvdpouw@gmail.com>
2022-10-22 12:02:25 +0200

    create compontent structure

---
+++ web/layouts/default.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: "MasterLayout",
+};
+</script>
+<template>
+  <h1>template hello</h1>
+  <slot />
+</template>
---
+++ web/pages/index.vue
@@ -0,0 +1,8 @@
+<script>
+export default {
+  name: "LandingPage",
+};
+</script>
+<template>
+  <h1>hello word</h1>
+</template>
