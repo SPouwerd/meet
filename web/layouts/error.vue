f2403abfc82be1ffe546362ad0891dda549e908f
svan-derp <stijnvdpouw@gmail.com>
2022-11-02 17:13:02 +0100

    show errors on DOM

--- web/layouts/error.vue
+++ web/layouts/error.vue
@@ -1,10 +1,14 @@
 <template>
-  <div>
+  <div class="surface-card p-8 shadow-2 border-round">
     <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>
-      An error occurred - {{ error.statusCode }} <br />
-      {{ error.message }}
-    </h1>
+    <div v-else>
+      <h1 v-if="error.message">{{ error.message }}</h1>
+      <h2 v-if="error.statusCode">{{ error.statusCode }}</h2>
+      <h2 v-if="error.response.data.message">
+        {{ error.response.data.message }}
+      </h2>
+      <h4 v-if="error.response">{{ error.response }}</h4>
+    </div>
     <NuxtLink to="/">Home page</NuxtLink>
   </div>
 </template>
